2bf67d24c2aa39fdb8aa84617ae6c1a0fc241a54
lalumabeserra <lalumabeserra@lalumabeserras-MacBook-Pro.local>
2020-02-05 10:24:46 -0300

    Creates menu choice

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -4,16 +4,39 @@ import com.twu.biblioteca.repository.BookRepository;
 import com.twu.biblioteca.service.BookService;
 import com.twu.biblioteca.view.BibliotecaView;
 
+import java.util.Scanner;
+
 public class BibliotecaApp {
+    private BookRepository bookRepository;
+    private BookService bookService;
+    private BibliotecaView bibliotecaView;
 
-    public static void main(String[] args) {
+    public BibliotecaApp(){
+        bookRepository = new BookRepository();
+        bookService = new BookService(bookRepository);
+        bibliotecaView =  new BibliotecaView(bookService);
+    }
 
-        BookRepository bookRepository = new BookRepository();
-        BookService bookService = new BookService(bookRepository);
-        BibliotecaView bibliotecaView =  new BibliotecaView(bookService);
+    public void start(){
+        int menuOption = 0;
 
         System.out.println(bibliotecaView.showWelcomeMessage());
+        do {
+            bibliotecaView.showMenuOptions().forEach(option -> System.out.println(option));
 
-        bibliotecaView.showMenuOptions().forEach(option -> System.out.println(option));
+            Scanner scanner = new Scanner(System.in);
+            menuOption = scanner.nextInt();
+
+            selectMenuOption(menuOption);
+        } while(menuOption != -1);
+    }
+
+    public void selectMenuOption(int option){
+        switch (option){
+            case 1: bibliotecaView.showBooks().forEach(book -> System.out.println(book));
+                    break;
+            default: System.out.println("Please select a valid option!");
+                    break;
+        }
     }
 }
---
+++ src/com/twu/biblioteca/BibliotecaMain.java
@@ -0,0 +1,9 @@
+package com.twu.biblioteca;
+
+public class BibliotecaMain {
+
+    public static void main(String[] args) {
+        BibliotecaApp bibliotecaApp = new BibliotecaApp();
+        bibliotecaApp.start();
+    }
+}
